4dd03c0a75a5d87ca9f2e15681f0e5b023a67f44
Epikle <98042337+Epikle@users.noreply.github.com>
2022-12-05 19:26:43 +0200

    Day 5

---
+++ 5/part2.js
@@ -0,0 +1,33 @@
+import { example, data } from './data.js';
+
+const [stacks, moves] = data.split('\n\n');
+
+const crates = stacks
+  .replace(/^\s*\n/gm, '')
+  .split('\n')
+  .reverse()
+  .slice(1);
+
+const cratesArray = [];
+
+for (let i = 0; i < crates[0].split(' ').length; i++) {
+  cratesArray.push(crates.map((c) => c.replaceAll('    ', ' ').split(' ')[i]));
+}
+
+const cleanedArray = cratesArray.map((r) => r.filter((c) => c != ''));
+const cleanedMoves = moves.split('\n');
+
+cleanedMoves.forEach((move) => {
+  const [, amount, , from, , to] = move.split(' ');
+
+  const moving = cleanedArray[+from - 1].splice(+amount * -1);
+  cleanedArray[+to - 1] = cleanedArray[+to - 1].concat(moving);
+});
+
+const answer = [];
+
+cleanedArray.forEach((r) => {
+  answer.push(r.slice(-1));
+});
+
+console.log(answer.join('').replace(/[\[\]]/g, ''));
