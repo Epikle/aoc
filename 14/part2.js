ff97bf41bc44c9a0abb64a00ad662c4d9827a0dd
Epikle <98042337+Epikle@users.noreply.github.com>
2022-12-14 20:52:02 +0200

    Day 14

---
+++ 14/part2.js
@@ -0,0 +1,66 @@
+import { example, data } from './data.js';
+
+const lines = data
+  .split('\n')
+  .map((points) =>
+    points.split(' -> ').map((point) => point.split(',').map(Number))
+  );
+const maxX = Math.max(...lines.flat(1).map((l) => l[0])) * 2;
+const maxY = Math.max(...lines.flat(1).map((l) => l[1]));
+const cave = Array.from({ length: maxY + 3 }, () => Array(maxX).fill(0));
+
+lines.push([
+  [0, maxY + 2],
+  [maxX * 2, maxY + 2],
+]);
+
+lines.forEach((points) =>
+  points.forEach((point, index) => {
+    if (index !== 0) {
+      const min = [
+        Math.min(points[index - 1][0], point[0]),
+        Math.min(points[index - 1][1], point[1]),
+      ];
+      const max = [
+        Math.max(points[index - 1][0], point[0]),
+        Math.max(points[index - 1][1], point[1]),
+      ];
+
+      for (let i = min[0]; i <= max[0]; i++) {
+        for (let j = min[1]; j <= max[1]; j++) {
+          cave[j][i] = 1;
+        }
+      }
+    }
+  })
+);
+
+let [x, y] = [500, 0];
+let units = 0;
+
+while (true) {
+  if (cave[y + 1][x] === 0) {
+    y++;
+    continue;
+  }
+  if (cave[y + 1][x - 1] === 0) {
+    y++;
+    x--;
+    continue;
+  }
+  if (cave[y + 1][x + 1] === 0) {
+    y++;
+    x++;
+    continue;
+  }
+
+  units++;
+  cave[y][x] = 2;
+
+  if (y === 0) break;
+
+  x = 500;
+  y = 0;
+}
+
+console.log({ answer: units });
